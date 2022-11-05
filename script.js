973f36caafb6735b6a7e40c1928dfa7dee33ff54
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-11-06 00:24:43 +0200

    mai am putin

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ const currentScore2 = document.querySelector('#current--1');
 const scorePlayer1 = document.querySelector('#score--0');
 const scorePlayer2 = document.querySelector('#score--1');
 
-currentScore1.textContent = 0;
-currentScore2.textContent = 0;
-scorePlayer1.textContent = 0;
-scorePlayer2.textContent = 0;
-
 let scoreP1 = 0;
 let scoreP2 = 0;
 
@@ -30,13 +25,17 @@ function changeDiceImage(image) {
 }
 
 // count score
-function countScore(number) {
+function countCurrentScore(number) {
   if (playerActive1.classList.contains('player--active')) {
-    // aici conditia de win !
     currentScore1.textContent = scoreP1 += number;
+    scoreP1 > 60
+      ? console.log('Player 1 Wins')
+      : console.log('Continue playing');
   } else if (playerActive2.classList.contains('player--active')) {
-    // aici conditia de win !
     currentScore2.textContent = scoreP2 += number;
+    scoreP2 > 60
+      ? console.log('Player 2 Wins')
+      : console.log('Continue playing');
   }
 }
 
@@ -44,12 +43,14 @@ function countScore(number) {
 
 function rollDice() {
   const dice = Math.trunc(Math.random() * 6) + 1;
-  countScore(dice);
+  countCurrentScore(dice);
   if (dice === 1) {
     changeDiceImage('dice-1.png');
-    switchPlayersOne();
-    scorePlayer1.textContent = 0;
-    scorePlayer2.textContent = 0;
+    switchPlayersOnDiceOne();
+    // scorePlayer1.textContent = scoreP1 += number;
+    // scorePlayer2.textContent = scoreP2 += number;
+    currentScore1.textContent = 0;
+    currentScore2.textContent = 0;
   } else if (dice === 2) {
     changeDiceImage('dice-2.png');
   } else if (dice === 3) {
@@ -72,15 +73,21 @@ function switchPlayers() {
     playerActive2.classList.add('player--active');
     playerActive1.classList.remove('player--active');
     scorePlayer1.textContent = currentScore1.textContent;
+    // console.log('player 2');
+    currentScore1.textContent = 0;
+    scoreP1 = 0;
   } else {
     playerActive2.classList.remove('player--active');
     playerActive1.classList.add('player--active');
     scorePlayer2.textContent = currentScore2.textContent;
+    //   console.log('player 1');
+    currentScore.textContent = 0;
+    scoreP2 = 0;
   }
 }
 
 // schimba jucatorul cand da jarul 1
-function switchPlayersOne() {
+function switchPlayersOnDiceOne() {
   if (playerActive1.classList.contains('player--active')) {
     playerActive2.classList.add('player--active');
     playerActive1.classList.remove('player--active');
